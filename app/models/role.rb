a9591d02e45cb7de6b43a1f7a210ab5d6e8a3f38
OdongAlican <sandieo.2020@gmail.com>
2021-09-15 01:28:29 +0300

    add student and teacher association

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,4 +3,8 @@
 class Role < ApplicationRecord
   has_many :manages, dependent: :destroy
   has_many :permissions, through: :manages, dependent: :destroy
+  has_many :student_roles, dependent: :destroy
+  has_many :students, through: :student_roles, dependent: :destroy
+  has_many :teacher_roles, dependent: :destroy
+  has_many :teachers, through: :teacher_roles, dependent: :destroy
 end
