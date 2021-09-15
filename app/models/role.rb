079a87541b7d5560a46c783ec6e576cd0ab2ac9f
OdongAlican <sandieo.2020@gmail.com>
2021-09-15 16:50:32 +0300

    add staffs association

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Role < ApplicationRecord
+  has_many :staffs
   has_many :manages, dependent: :destroy
   has_many :permissions, through: :manages, dependent: :destroy
   has_many :student_roles, dependent: :destroy
