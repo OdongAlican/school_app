101e0e739f0a8e3177719e8c857172b37183a9be
OdongAlican <sandieo.2020@gmail.com>
2021-09-16 21:02:45 +0300

    add supers controller

---
+++ app/controllers/supers_controller.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class SupersController < ApplicationController
+  before_action :find_super, only: %i[show]
+
+  def index
+    @supers = Super.all
+    json_response(@supers, :created)
+  end
+
+  def create
+    @super = Super.create(super_params)
+    if @super.save
+      json_response(@super, :created)
+    else
+      json_response('Something went wrong')
+    end
+  end
+
+  def update
+    if @super.update(super_params)
+      json_response(@super, :created)
+    else
+      json_response('Something went wrong')
+    end
+  end
+
+  def show
+    result = @super.to_json({ include: { roles: { include: 'permissions' } } })
+    json_response(result)
+  end
+
+  private
+
+  def find_super
+    @super = Super.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    json_response('Record Not Found')
+  end
+
+  def super_params
+    params.permit(:first_name, :last_name, :email, :password, role_ids: [])
+  end
+end
