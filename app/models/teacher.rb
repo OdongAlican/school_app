ca9ef2a7f60b4866b2b9f15c968e1d39c421fa65
OdongAlican <sandieo.2020@gmail.com>
2021-09-16 15:50:23 +0300

    add number of streams validation method

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,25 @@
 # frozen_string_literal: true
 
 class Teacher < ApplicationRecord
+  validate :validate_streams_per_semester
   has_many :teacher_roles, dependent: :destroy
   has_many :roles, through: :teacher_roles, dependent: :destroy
+  has_many :teacher_streams, dependent: :destroy
+  has_many :streams, through: :teacher_streams, dependent: :destroy
+
+  def validate_streams_per_semester
+    if streams.length > 3
+      count = 0
+      (0...streams.length).each do |i|
+        count = 0
+        value = streams[i].semester_id
+        (0...streams.length).each do |j|
+          next unless value == streams[j].semester_id
+
+          count += 1
+          errors.add(:streams, :invalid) if count > 3
+        end
+      end
+    end
+  end
 end
