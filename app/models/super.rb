18df900e2cfe623a97cc845417ba918d743f9c94
OdongAlican <sandieo.2020@gmail.com>
2021-09-18 14:22:30 +0300

    update super model

--- app/models/super.rb
+++ app/models/super.rb
@@ -3,13 +3,4 @@
 class Super < ApplicationRecord
   has_many :super_roles, dependent: :destroy
   has_many :roles, through: :super_roles, dependent: :destroy
-
-  def self.users
-    sql = " SELECT * INTO users
-            FROM ( SELECT password, email FROM students UNION ALL
-                  SELECT password, email FROM teachers UNION ALL
-                  SELECT password, email FROM staffs UNION ALL
-                  SELECT password, email FROM streams )"
-    ActiveRecord::Base.connection.execute(sql)
-  end
 end
