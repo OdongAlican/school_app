5b8f9710b3b5fd2a4a022704fa08d5d1ab61aeee
OdongAlican <sandieo.2020@gmail.com>
2021-09-18 23:56:32 +0300

    update student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,6 +6,8 @@ class Student < ApplicationRecord
   has_many :roles, through: :student_roles, dependent: :destroy
   has_many :student_streams, dependent: :destroy
   has_many :streams, through: :student_streams, dependent: :destroy
+  has_many :principle_students, dependent: :destroy
+  has_many :principles, through: :principle_students, dependent: :destroy
 
   def validate_streams_per_semester
     if streams.length > 6
