bf3c53618dc6351ba196f0088ce7f29e732b9a6f
OdongAlican <sandieo.2020@gmail.com>
2021-09-18 14:20:28 +0300

    add determine_type method

--- app/controllers/concerns/authorize.rb
+++ app/controllers/concerns/authorize.rb
@@ -8,4 +8,24 @@ module Authorize
     end
     roles_list
   end
+
+  def determine_type(current_user)
+    if current_user.class.name == 'Teacher'
+      teacher = Teacher.find_by(id: current_user['id'])
+      @user_roles_list = user_roles(teacher)
+    elsif current_user.class.name == 'Student'
+      student = Student.find_by(id: current_user['id'])
+      @user_roles_list = user_roles(student)
+    elsif current_user.class.name == 'Principle'
+      principle = Principle.find_by(id: current_user['id'])
+      @user_roles_list = user_roles(principle)
+    elsif current_user.class.name == 'Staff'
+      result = Staff.find_by(id: current_user['id'])
+      role = Role.find_by(id: result.role_id)
+      @user_roles_list = [role.name]
+    elsif current_user.class.name == 'Super'
+      super_user = Super.find_by(id: current_user['id'])
+      @user_roles_list = user_roles(super_user)
+    end
+  end
 end
