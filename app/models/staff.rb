12d5a788fb8ffca233c319ddd90854149ffee025
OdongAlican <sandieo.2020@gmail.com>
2021-09-18 23:55:54 +0300

    update staff model

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -2,4 +2,6 @@
 
 class Staff < ApplicationRecord
   belongs_to :role, dependent: :destroy
+  has_many :principle_staffs, dependent: :destroy
+  has_many :principles, through: :principle_staffs, dependent: :destroy
 end
