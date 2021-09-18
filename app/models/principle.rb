8d82d84565c20365d22e0472699fcf51bae3c627
OdongAlican <sandieo.2020@gmail.com>
2021-09-18 23:55:07 +0300

    update principle model

--- app/models/principle.rb
+++ app/models/principle.rb
@@ -3,4 +3,19 @@
 class Principle < ApplicationRecord
   has_many :principle_roles, dependent: :destroy
   has_many :roles, through: :principle_roles, dependent: :destroy
+
+  has_many :principle_semesters, dependent: :destroy
+  has_many :semesters, through: :principle_semesters, dependent: :destroy
+
+  has_many :principle_teachers, dependent: :destroy
+  has_many :teachers, through: :principle_teachers, dependent: :destroy
+
+  has_many :principle_students, dependent: :destroy
+  has_many :students, through: :principle_students, dependent: :destroy
+
+  has_many :principle_staffs, dependent: :destroy
+  has_many :staffs, through: :principle_staffs, dependent: :destroy
+
+  has_many :principle_streams, dependent: :destroy
+  has_many :streams, through: :principle_streams, dependent: :destroy
 end
