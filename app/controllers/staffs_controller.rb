360831b8c6ab296486a850f0b03a23ea08d67b41
OdongAlican <sandieo.2020@gmail.com>
2021-09-18 14:21:24 +0300

    update staffs controller

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -2,17 +2,23 @@
 
 class StaffsController < ApplicationController
   before_action :find_staff, only: %i[update show destroy]
+  before_action :authorized, except: %i[login]
+  before_action :user_roles_list, except: %i[login]
 
   def index
-    @staffs = Staff.all.limit(10).to_json({ include: 'role' })
-    json_response(@staffs, :created)
+    if (@user_roles_list & %w[Staff Admin Contractor]).any?
+      @staffs = Staff.all.limit(10).to_json({ include: 'role' })
+      json_response(@staffs, :created)
+    else
+      json_response('Not authorized', :unauthorized)
+    end
   end
 
   def login
     @staff = Staff.find_by(email: params[:email])
     password = params.permit(:password)
     if @staff && @staff.password == password['password']
-      token = encode_token({ staff_id: @staff.id }, 'staff')
+      token = encode_token({ staff_id: @staff.id })
       data = { staff: @staff, token: token }
       json_response(data, :ok)
     else
@@ -21,31 +27,61 @@ class StaffsController < ApplicationController
   end
 
   def create
-    @staff = Staff.create(staff_params)
-    @staff.password = random_password
-    if @staff.save
-      result = @staff.to_json({ include: 'role' })
-      json_response(result, :created)
+    if current_user.class.name == 'Principle' && @user_roles_list.include?('Admin')
+      @staff = Staff.create(staff_params)
+      @staff.password = random_password
+      if @staff.save
+        result = @staff.to_json({ include: 'role' })
+        json_response(result, :created)
+      else
+        json_response('Can not be create', :unprocessable_entity)
+      end
     else
-      json_response('Can not be create', :unprocessable_entity)
+      json_response('Not authorized', :unauthorized)
     end
   end
 
   def show
-    result = @staff.to_json({ include: 'role' })
-    json_response(result, :created)
+    if (@user_roles_list & %w[Staff Admin Contractor]).any?
+      result = @staff.to_json({ include: 'role' })
+      json_response(result, :created)
+    else
+      json_response('Not authorized', :unauthorized)
+    end
   end
 
   def update
-    if @staff.update(staff_params)
-      result = @staff.to_json(include: 'role')
-      json_response(result, :unprocessable_entity)
+    if (@user_roles_list & %w[Staff Admin Contractor]).any?
+      if @staff.update(staff_params)
+        result = @staff.to_json(include: 'role')
+        json_response(result, :unprocessable_entity)
+      else
+        json_response('Cannot update')
+      end
     else
-      json_response('Cannot update')
+      json_response('Not authorized', :unauthorized)
+    end
+  end
+
+  def destroy
+    if current_user.class.name == 'Principle' && @user_roles_list.include?('Admin')
+      if @staff.destroy
+        data = { 'message' => 'staff destroyed' }
+        json_response(data, :no_content)
+      else
+        data = { 'message' => 'Something went wrong' }
+        json_response(data, :no_content)
+      end
+    else
+      json_response('Not authorized', :unauthorized)
     end
   end
 
   private
+
+  def user_roles_list
+    determine_type(current_user)
+  end
 
   def find_staff
     @staff = Staff.find(params[:id])
@@ -57,3 +93,9 @@ class StaffsController < ApplicationController
     params.permit(:first_name, :last_name, :email, :password, :role_type, :role_id)
   end
 end
+
+# lorenzo@gmail.com
+# Lsu4t8hfm2
+
+# "email": "rosemary@gmail.com",
+# "password": "czQF8G1UPn",
