4f4579b7405aaeed5fdcb48015ecfbb43fc5d516
OdongAlican <sandieo.2020@gmail.com>
2021-09-18 04:17:12 +0300

    update controller

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,18 +2,25 @@
 
 class TeachersController < ApplicationController
   before_action :find_teacher, only: %i[update show destroy]
+  before_action :authorized, except: %i[login]
+  before_action :user_roles_list, except: %i[login]
 
   def index
-    @teachers = Teacher.all.limit(10).to_json({ include: { roles: { include: 'permissions' } } })
-    json_response(@teachers, :created)
+    if (@user_roles_list & %w[Staff Admin]).any?
+      @teachers = Teacher.all.limit(10).to_json({ include: { roles: { include: 'permissions' } } })
+      json_response(@teachers, :created)
+    else
+      json_response('Not authorized', :unauthorized)
+    end
   end
 
   def login
     @teacher = Teacher.find_by(email: params[:email])
     password = params.permit(:password)
     if @teacher && @teacher.password == password['password']
-      token = encode_token({ teacher_id: @teacher.id }, 'teacher')
-      data = { teacher: @teacher, token: token }
+      token = encode_token({ teacher_id: @teacher.id })
+      result = @teacher.to_json({ include: { roles: { include: 'permissions' } } })
+      data = { teacher: result, token: token }
       json_response(data, :ok)
     else
       json_response('Invalid teacher name or password', :unauthorized)
@@ -21,13 +28,17 @@ class TeachersController < ApplicationController
   end
 
   def create
-    @teacher = Teacher.create(teacher_params)
-    @teacher.password = random_password
-    if @teacher.save
-      result = @teacher.to_json({ include: { roles: { include: 'permissions' } } })
-      json_response(result, :created)
+    if @user_roles_list.include? 'Admin' && current_user.class.name == 'Principle'
+      @teacher = Teacher.create(teacher_params)
+      @teacher.password = random_password
+      if @teacher.save
+        result = @teacher.to_json({ include: { roles: { include: 'permissions' } } })
+        json_response(result, :created)
+      else
+        json_response('Error creating teacher', :unprocessable_entity)
+      end
     else
-      json_response('Error creating teacher', :unprocessable_entity)
+      json_response('Not authorized', :unauthorized)
     end
   end
 
@@ -56,6 +67,19 @@ class TeachersController < ApplicationController
   end
 
   private
+
+  def user_roles_list
+    if current_user.class.name == 'Teacher'
+      teacher = Teacher.find_by(id: current_user['id'])
+      @user_roles_list = user_roles(teacher)
+    elsif current_user.class.name == 'Student'
+      student = Student.find_by(id: current_user['id'])
+      @user_roles_list = user_roles(student)
+    elsif current_user.class.name == 'Principle'
+      principle = Principle.find_by(id: current_user['id'])
+      @user_roles_list = user_roles(principle)
+    end
+  end
 
   def find_teacher
     @teacher = Teacher.find(params[:id])
